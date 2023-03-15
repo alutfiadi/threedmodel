f103581d2f7d4772e6a9f0bb64015d41120622f0
alutfiadi <ahmadlutfiadi@gmail.com>
2023-03-15 13:48:57 +0700

    final

--- webapp/controller/Loader.controller.js
+++ webapp/controller/Loader.controller.js
@@ -3,27 +3,33 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/vk/ContentConnector",
 	"sap/ui/vk/thirdparty/three",
-	"sap/ui/vk/ZoomTo"
+	"sap/ui/vk/ZoomTo",
+    "sap/m/MessageBox"
 ], function(
 	MessageToast,
 	Controller,
 	ContentConnector,
 	threejs,
-	ZoomTo
+	ZoomTo,
+	MessageBox
 ) {
 	"use strict";
+	var sResponsivePaddingClasses = "sapUiResponsivePadding--header sapUiResponsivePadding--content sapUiResponsivePadding--footer";
 
 	return Controller.extend("threedmodel.controller.Loader", {
-
+		
 		onInit: function() {
 			var view = this.getView();
 			var viewer = this.viewer = view.byId("viewer");
+			var that = this;
 			//fire node clicked 
 			viewer.attachNodeClicked(function(event) {
 				var nodeRef = event.getParameters().nodeRef;
 				var id = nodeRef.id;
 				console.log("NODEREF: \n",nodeRef.id)
 				// console.log("NODEREF POSITION: \n",nodeRef.position);
+				
+				that.onShowDetail(nodeRef.id);
 			});
 			
 			var colladaLoader = function(parentNode, contentResource) {
@@ -43,6 +49,7 @@ sap.ui.define([
 									node: parentNode,
 									contentResource: contentResource
 								});
+								
 							},
 							null,  // onprogress
 							reject // onfail
@@ -67,10 +74,26 @@ sap.ui.define([
 
 			var viewer = this.getView().byId("viewer");
 			viewer.attachSceneLoadingSucceeded(function() {
+				//viewer.setEnableSceneTree(false);
+				viewer.setShowSceneTree(false);
 				viewer.getViewport().zoomTo([ ZoomTo.Visible, ZoomTo.ViewTop ], null, 0);
 			});
+
 			
-			
-		}
+		},
+		onShowDetail: function (node) {
+			MessageBox.information("Detail Information.", {
+				title: "Information",
+				id: "messageBoxId1",
+				details: "<p><strong>Building:</strong></p>" +
+					"<ul>" +
+					"<li>ID " + node + "</li>" +
+					"<li>Name : GOP 9 Building</em></li>" +
+					"<li>Status : <strong>Vacant</strong></em></li>" +
+					"</ul>" ,
+				contentWidth: "100px",
+				styleClass: sResponsivePaddingClasses
+			});
+		},
 	});
 });
